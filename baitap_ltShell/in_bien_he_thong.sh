39d7cc6ca888a663843c49e7ff86d186623cc9a9
root <root@localhost.localdomain>
2022-01-05 16:23:43 +0700

    cac bai tap da lam duoc

---
+++ baitap_ltShell/in_bien_he_thong.sh
@@ -0,0 +1,15 @@
+# My first shell script 
+#clear
+echo "Hello $USER"
+echo "Các biến hệ thống của bạn là"
+echo "BASH VERSION : $BASH_VERSION"
+echo "COLUMNS: $COLUMNS"
+echo "HOME: $HOME"
+echo "LINES: $LINES"
+echo "LOGNAME $LOGNAME"
+echo "OSTYPE: $OSTYPE"
+echo "PATH: $PATH"
+echo "PS1: $PS1"
+echo "PWD: $PWD"
+echo "SHELL: $SHELL"
+
